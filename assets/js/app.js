aee4c9951263e7526def316d41e6ff977e550915
Todd Croak-Falen <toddcf@gmail.com>
2019-02-22 15:14:45 -0800

    Added animations on scroll.

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,4 +8,57 @@ $( document ).ready( function() {
     $( '.currentYear' ).html( '&ndash;&nbsp;' + thisYear + '&nbsp;' );
   }) ();
 
+  // ANIMATIONS ON SCROLL
+  // Not actually using "direction," because this animation will only happen once.
+
+  // My Portfolio
+  $( '#my-portfolio-project' ).waypoint( function( direction ) {
+    $( '#my-portfolio-project' ).addClass( 'animate-1s fadeInUp' );
+  }, {
+    offset: '85%'
+  });
+
+  // Omnifood
+  $( '#omnifood-project' ).waypoint( function( direction ) {
+    $( '#omnifood-project' ).addClass( 'animate-1s animate-delay-a fadeInUp' );
+  }, {
+    offset: '85%'
+  });
+
+  // Author Site
+  $( '#author-site-project' ).waypoint( function( direction ) {
+    $( '#author-site-project' ).addClass( 'animate-1s animate-delay-b fadeInUp' );
+  }, {
+    offset: '85%'
+  });
+
+  // Footer Icons
+  // About Me Card
+  $( '#about-me-card' ).waypoint( function( direction ) {
+    $( '#about-me-card' ).addClass( 'animate-1s bounceInUp' );
+  }, {
+    offset: '85%'
+  });
+
+  // GitHub Card
+  $( '#github-card' ).waypoint( function( direction ) {
+    $( '#github-card' ).addClass( 'animate-1s bounceInUp' );
+  }, {
+    offset: '85%'
+  });
+
+  // LinkedIn Card
+  $( '#linkedin-card' ).waypoint( function( direction ) {
+    $( '#linkedin-card' ).addClass( 'animate-1s bounceInUp' );
+  }, {
+    offset: '85%'
+  });
+
+  // Resume Card
+  $( '#resume-card' ).waypoint( function( direction ) {
+    $( '#resume-card' ).addClass( 'animate-1s bounceInUp' );
+  }, {
+    offset: '85%'
+  });
+
 } );
